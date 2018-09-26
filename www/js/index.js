0a67dd02c76d01da6616979044f4ab848bba1ac5
Chris Bradley <chris@cbxiii.com>
2018-09-26 12:23:51 +0100

    cleaning up javascript

--- www/js/index.js
+++ www/js/index.js
@@ -5,7 +5,6 @@ var app = {
     },
 
     // deviceready Event Handler
-    //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
@@ -14,15 +13,10 @@ var app = {
       
       // Read NDEF formatted NFC Tags
       nfc.readerMode(
-          nfc.FLAG_READER_NFC_A | nfc.FLAG_READER_NO_PLATFORM_SOUNDS, 
-          //nfcTag => console.log(JSON.stringify(nfcTag)),
-          //nfcTag => console.log(nfc.bytesToHexString(nfcTag.ndefMessage[0].payload));
-          nfcReadSuccess
-          ,
-          //nfcTag => console.log('connected to', nfc.bytesToHexString(nfcEvent.tag.id))
+          nfc.FLAG_READER_NFC_A,
+          nfcReadSuccess,
           error => console.log('NFC reader mode failed', error)
       );
-
 
       function nfcReadSuccess(nfcTag) {
 
@@ -47,8 +41,6 @@ var app = {
         $("h2#waiting").html("Hello " + runnerName);
         $("p#scanned-info").html("Runner ID " + runnerNum);
 
-
-        //console.log("Tag data is " + tagContentString);
       } 
 
       function hex2a(hexx) {
@@ -59,36 +51,6 @@ var app = {
           return str;
       }
 
-     /* nfc.addTagDiscoveredListener (
-          function (nfcEvent) {
-            
-              $(".scan-area").css("background", "lightgreen");
-              $("h2#waiting").html("Success");
-              $("p#scanned-info").html("Well done");
-            
-            console.log(nfcEvent);
-              
-              var tag = nfcEvent.tag,
-              ndefMessage = tag.ndefMessage;
-              //console.log(tag);
-              //console.log(ndefMessage);
-      
-              // dump the raw json of the message
-              // note: real code will need to decode
-              // the payload from each record
-              //console.log (JSON.stringify(ndefMessage));
-      
-              // assuming the first record in the message has
-              // a payload that can be converted to a string.
-              //console.log(nfc.bytesToString(ndefMessage[0].payload).substring(3));
-          },
-          function () { // success callback
-              console.log("Ready to scan");
-          },
-          function (error) { // error callback
-              console.log("Error adding NDEF listener " + JSON.stringify(error));
-          }
-      );*/
     },
 
     // Update DOM on a Received Event
